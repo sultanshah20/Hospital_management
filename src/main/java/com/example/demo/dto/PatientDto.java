fb1850fb58f87fc630a4fa8e4ff73122b99f7336
sulatnshah20 <sultanshah26893@gmail.com>
2024-07-05 10:59:28 +0530

    this all application on github

---
+++ src/main/java/com/example/demo/dto/PatientDto.java
@@ -0,0 +1,36 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+import com.example.demo.entity.Patient;
+import com.example.demo.entity.Problem;
+import com.example.demo.entity.enums.City;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PatientDto implements Serializable {
+	private Long patientid;
+	private String name;
+	private String lastname;
+	private String phoneNo;
+	private Date bornDate;
+	private String gender;
+	private City city;
+	private String email;
+	private int status;
+	
+	private List<ProblemDtoForPatientSingleDto> problems;
+}
---
+++ src/main/java/com/example/demo/entity/Receipe.java
@@ -0,0 +1,49 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.example.demo.entity.enums.ProblemStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="aareceipe")
+public class Receipe {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AA_PATIENT_SEQ")
+    @SequenceGenerator(sequenceName = "AA_PATIENT_SEQ", allocationSize = 1, name = "AA_PATIENT_SEQ")
+	@Column(name = "receipeid")
+	private Long receipeid;
+	
+	private String detail;
+	private String barcode;
+	private String drug_detail;
+	private String usage;
+	private String delivery_date;
+	private Long problemid;
+	private Long patientid;
+	private int status;
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id")
+    private Problem problem;
+	
+}
